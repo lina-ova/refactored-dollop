0962a8e2ca38076c09ec84bfcdeb78b35dab5113
Polina <nazpolina00@gmail.com>
2024-10-26 17:32:35 +0300

    initial

---
+++ index.js
@@ -0,0 +1,99 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+var morgan = require('morgan')
+app.use(morgan('tiny'))
+const cors = require('cors')
+
+app.use(cors())
+
+let persons = [
+    {
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: "1"
+    },
+    {
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+        id: "2"
+    },
+    {
+        name: "Joonas",
+        phone: "76445",
+        id: "3"
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/info', (request, response) => {
+    response.send(`<div>
+        <p>Phonebook has ${persons.length} people</p>
+        <p>${Date(Date.now()).toString()}</p>
+        </div>`)
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    console.log(body)
+    if (!body.name || !body.phone) {
+        return response.status(400).json({
+            error: 'name or phone missing'
+        })
+    }
+    if (persons.map(person => person.name).includes(body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(n => Number(n.id)))
+        : 0
+
+    const person = {
+        name: body.name,
+        phone: body.phone,
+        id: String(maxId + 1),
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})

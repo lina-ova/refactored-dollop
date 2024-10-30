975c826434bb795aa60e9a9c63c872751f3ba1d8
Polina <nazpolina00@gmail.com>
2024-10-30 17:50:17 +0200

    refactoring

---
+++ controllers/persons.js
@@ -0,0 +1,62 @@
+const personsRouter = require('express').Router()
+const Person = require('../models/person')
+
+
+
+personsRouter.get('/', (request, response) => {
+  Person.find({}).then(people => {
+    response.json(people)
+  })
+})
+
+personsRouter.get('/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+personsRouter.post('/', (request, response, next) => {
+  const body = request.body
+
+  const person = new Person({
+    name: body.name,
+    phone: body.phone,
+  })
+
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
+})
+
+personsRouter.delete('/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+personsRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    phone: body.phone,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = personsRouter
